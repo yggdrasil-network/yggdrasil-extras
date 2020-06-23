40a3b10819e8a32dbe1ba7c3258ee9af8e96cc7d
vadym <vadym.vikulin@gmail.com>
2020-06-23 05:57:15 -0700

    1. first stop fix

--- src/mobile/mobile.go
+++ src/mobile/mobile.go
@@ -112,9 +112,6 @@ func (m *Yggdrasil) StartJSON(configjson []byte) (conduit *dummy.ConduitEndpoint
 // Stop the mobile Yggdrasil instance
 func (m *Yggdrasil) Stop() error {
 	m.core.Stop()
-	if err := m.Stop(); err != nil {
-		return err
-	}
 	return nil
 }
 
