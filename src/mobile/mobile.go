cafd117c37b2a53447dbdacca1ad4e66fd4a7742
vadym <vadym.vikulin@gmail.com>
2020-06-23 05:01:43 -0700

    1. An anothe Yggdrasil stop fix

--- src/mobile/mobile.go
+++ src/mobile/mobile.go
@@ -111,7 +111,9 @@ func (m *Yggdrasil) StartJSON(configjson []byte) (conduit *dummy.ConduitEndpoint
 
 // Stop the mobile Yggdrasil instance
 func (m *Yggdrasil) Stop() error {
-	m.core.Stop()
+	if err := m.Stop(); err != nil {
+		return err
+	}
 	return nil
 }
 
