4645e9d79dfc3d5833a156bb571d3e9160646313
Neil Alexander <neilalexander@users.noreply.github.com>
2021-06-15 09:37:29 +0100

    Clip MTU to MaxMTU properly

--- src/mobile/mobile.go
+++ src/mobile/mobile.go
@@ -56,7 +56,11 @@ func (m *Yggdrasil) StartJSON(configjson []byte) error {
 		logger.Errorln("An error occured starting Yggdrasil:", err)
 		return err
 	}
-	m.core.SetMTU(m.config.IfMTU)
+	mtu := tun.config.IfMTU
+	if m.core.MaxMTU() < mtu {
+		mtu = m.core.MaxMTU()
+	}
+	m.core.SetMTU(mtu)
 	if err := m.multicast.Init(&m.core, &m.config, logger, nil); err != nil {
 		logger.Errorln("An error occurred initialising multicast:", err)
 		return err
