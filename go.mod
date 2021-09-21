ec183d8e22e1ea7c26a5ec233d88114fecfc95fc
Neil Alexander <neilalexander@users.noreply.github.com>
2021-09-21 20:16:42 +0100

    Fix builds (closes #14)

--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.16
 
 require (
 	github.com/gologme/log v1.2.0
-	github.com/hjson/hjson-go v3.1.0+incompatible
-	github.com/mitchellh/mapstructure v1.4.1
-	github.com/yggdrasil-network/yggdrasil-go v0.3.17-0.20210627222446-de853fed10ad
+	github.com/yggdrasil-network/yggdrasil-go v0.4.1-0.20210807175636-3613614b418d
 	golang.org/x/mobile v0.0.0-20210527171505-7e972142eb43
 )
--- src/mobile/mobile_ios.go
+++ src/mobile/mobile_ios.go
@@ -25,37 +25,3 @@ func (nsl MobileLogger) Write(p []byte) (n int, err error) {
 	C.Log(cstr)
 	return len(p), nil
 }
-
-/*
-func (c *Core) AWDLCreateInterface(name, local, remote string, incoming bool) error {
-	if intf, err := c.link.awdl.create(name, local, remote, incoming); err != nil || intf == nil {
-		c.log.Println("c.link.awdl.create:", err)
-		return err
-	}
-	return nil
-}
-
-func (c *Core) AWDLShutdownInterface(name string) error {
-	return c.link.awdl.shutdown(name)
-}
-
-func (c *Core) AWDLRecvPacket(identity string) ([]byte, error) {
-	if intf := c.link.awdl.getInterface(identity); intf != nil {
-		read, ok := <-intf.rwc.toAWDL
-		if !ok {
-			return nil, errors.New("AWDLRecvPacket: channel closed")
-		}
-		return read, nil
-	}
-	return nil, errors.New("AWDLRecvPacket identity not known: " + identity)
-}
-
-func (c *Core) AWDLSendPacket(identity string, buf []byte) error {
-	packet := append(util.GetBytes(), buf[:]...)
-	if intf := c.link.awdl.getInterface(identity); intf != nil {
-		intf.rwc.fromAWDL <- packet
-		return nil
-	}
-	return errors.New("AWDLSendPacket identity not known: " + identity)
-}
-*/
